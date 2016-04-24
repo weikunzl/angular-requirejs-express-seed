396b774fe1356a8380d2a7465653ab5c96b0ccd8
weikunzl@163.com <weikunzl@163.com>
2016-04-24 22:26:54 +0800

    “压缩”

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -33,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(config.host+'/', routes);
 app.use(config.host+'/users', users);
